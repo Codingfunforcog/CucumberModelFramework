346b7f0d7ac9e275e30625e2dddf5eabeef279aa
Sameer Patankar <sameer@example.com>
2013-01-24 15:11:15 +0530

    Commiting changes to Development

--- src/test/java/net/masterthought/example/ATMTest.java
+++ src/test/java/net/masterthought/example/ATMTest.java
@@ -4,8 +4,6 @@ import cucumber.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@Cucumber.Options(format = {"pretty", "html:target/cucumber", "json:target/cucumber.json"})
+@Cucumber.Options(format = {"pretty", "html:target/cucumber", "json:target/cucumber.json"}, tags = {"@pallavi1"})
 public class ATMTest {
-	
-	
 }
---
+++ src/test/resources/net/masterthought/example/ATM1.feature
@@ -0,0 +1,7 @@
+Feature: Pallavi's Google search
+
+@pallavi1
+Scenario: Pallavi search google
+Given Pallavi opens google home page
+And Pallavi type hindi songs in search
+Then Pallavi should get search results
